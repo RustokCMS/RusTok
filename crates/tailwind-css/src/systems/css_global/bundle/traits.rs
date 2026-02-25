62fb4a0eb25491ec817fdf6b0e3e0c13ef4cb9a1
loid345 <volanar@tut.by>
2026-02-26 00:22:28 +0300

    del

--- crates/tailwind-css/src/systems/css_global/bundle/traits.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-#[allow(clippy::derived_hash_with_manual_eq)]
-impl Hash for CssBundle {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.attribute.hash(state);
-        self.addition.hash(state);
-    }
-}
